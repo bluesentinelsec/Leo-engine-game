e394cbeba1a24fb53d5fb28fe13823527838a4b7
bluesentinelsec <bluesentinel@protonmail.com>
2025-10-24 19:48:22 -0400

    added CLI for window modes

--- src/main.c
+++ src/main.c
@@ -11,6 +11,7 @@ typedef struct
     int rect_width;
     int rect_height;
     bool one_frame;
+    leo_WindowMode window_mode;
 } GameState;
 
 static bool game_setup(leo_GameContext *ctx)
@@ -57,7 +58,12 @@ static void game_shutdown(leo_GameContext *ctx)
 
 static void print_usage(const char *prog)
 {
-    fprintf(stderr, "Usage: %s [--one-frame]\n", prog);
+    fprintf(stderr, "Usage: %s [OPTIONS]\n", prog);
+    fprintf(stderr, "Options:\n");
+    fprintf(stderr, "  --one-frame, -1      Run for one frame only\n");
+    fprintf(stderr, "  --windowed, -w       Start in windowed mode\n");
+    fprintf(stderr, "  --fullscreen, -f     Start in fullscreen exclusive mode (default)\n");
+    fprintf(stderr, "  --borderless, -b     Start in borderless fullscreen mode\n");
 }
 
 int main(int argc, char **argv)
@@ -66,20 +72,33 @@ int main(int argc, char **argv)
         .rect_width = 240,
         .rect_height = 140,
         .one_frame = false,
+        .window_mode = LEO_WINDOW_MODE_FULLSCREEN_EXCLUSIVE,
     };
 
     static struct option long_opts[] = {
         {"one-frame", no_argument, NULL, '1'},
+        {"windowed", no_argument, NULL, 'w'},
+        {"fullscreen", no_argument, NULL, 'f'},
+        {"borderless", no_argument, NULL, 'b'},
         {NULL, 0, NULL, 0},
     };
 
     int opt;
-    while ((opt = getopt_long(argc, argv, "1", long_opts, NULL)) != -1)
+    while ((opt = getopt_long(argc, argv, "1wfb", long_opts, NULL)) != -1)
     {
         switch (opt)
         {
         case '1':
             state.one_frame = true;
+            break;
+        case 'w':
+            state.window_mode = LEO_WINDOW_MODE_WINDOWED;
+            break;
+        case 'f':
+            state.window_mode = LEO_WINDOW_MODE_FULLSCREEN_EXCLUSIVE;
+            break;
+        case 'b':
+            state.window_mode = LEO_WINDOW_MODE_BORDERLESS_FULLSCREEN;
             break;
         case '?':
         default:
@@ -92,6 +111,7 @@ int main(int argc, char **argv)
         .window_width = 1280,
         .window_height = 720,
         .window_title = "Leo Engine Pong",
+        .window_mode = state.window_mode,
         .target_fps = 60,
         .logical_width = 1280,
         .logical_height = 720,
